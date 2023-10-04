592f1a3210e32a881d4d43d87284c149a9af7f67
evafigueiredo8 <144215024+evafigueiredo8@users.noreply.github.com>
2023-10-04 16:44:02 +0200

    Add files via upload

--- CreerEntier.java
+++ CreerEntier.java
@@ -1,8 +1,26 @@
 public class CreerEntier{
     private int valeur;
+    private int borneMin;
+    private int borneMax;
 
     public CreerEntier(int borneMin, int borneMax, int valeur){
-        if (valeur>borneMin) && (valeur>borneMax){
+        this.borneMin = borneMin;
+        this.borneMax = borneMax;
+        if ((valeur>borneMin) && (valeur>borneMax)){
+            this.valeur = valeur;
+        }
+    }
+
+    public CreerEntier(int borneMin, int borneMax){
+        valeur = 0;
+    }
+
+    public void getbornes(){
+        System.out.println(borneMin + ", " + borneMax);
+    }
+
+    public void setvaleur(int valeur){
+        if ((valeur>borneMin) && (valeur>borneMax)){
             this.valeur = valeur;
         }
     }
